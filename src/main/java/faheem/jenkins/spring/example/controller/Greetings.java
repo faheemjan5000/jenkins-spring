0fbe613f0966b35b68e103439acb660078cd7e0f
ITMASTER\MMF.Jan <mmf.jan@reative-almaviva.it>
2023-09-26 22:19:18 +0200

    first commit

---
+++ src/main/java/faheem/jenkins/spring/example/controller/Greetings.java
@@ -0,0 +1,15 @@
+package faheem.jenkins.spring.example.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/greetings")
+public class Greetings {
+
+    @GetMapping("/")
+    public String greetings(){
+        return "Good Morning!";
+    }
+}
---
+++ src/test/java/faheem/jenkins/spring/example/JenkinsSpringExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package faheem.jenkins.spring.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsSpringExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
